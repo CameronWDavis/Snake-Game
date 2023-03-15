fadffa2338cf4ed66ba412d5373fab77019077a9
CameronWDavis <77825507+CameronWDavis@users.noreply.github.com>
2023-03-14 22:06:37 -0400

    Update SnakeFrame.java

--- src/SnakeFrame.java
+++ src/SnakeFrame.java
@@ -6,7 +6,7 @@ public class SnakeFrame extends JFrame{
 //Constructor for the Frame
 	SnakeFrame(){
 		// Sets the Frame to the Panel 
-		this.add(new SnakePanel()); 
+		this.add(new SnakePanel()); //create the new snake panel to get the game going
 		this.setTitle("SNAKE");
 		this.pack();
 		this.setVisible(true);
